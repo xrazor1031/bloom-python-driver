e328f1d9e455bedcf414a5f5d155dd839e9446d1
gzen <xrazor1031@gmail.com>
2019-09-04 18:25:58 +0800

    refactor: comatiable with python 3

--- pybloomd.py
+++ pybloomd.py
@@ -63,9 +63,9 @@ class BloomdConnection(object):
         if self.sock is None:
             self.sock = self._create_socket()
         sent = False
-        for attempt in xrange(self.attempts):
+        for attempt in range(self.attempts):
             try:
-                self.sock.sendall(cmd + "\n")
+                self.sock.sendall(bytes(cmd + "\n", encoding="utf8"))
                 sent = True
                 break
             except socket.error as e:
@@ -114,7 +114,7 @@ class BloomdConnection(object):
         and reads the response, performing a retry if necessary.
         """
         done = False
-        for attempt in xrange(self.attempts):
+        for attempt in range(self.attempts):
             try:
                 self.send(cmd)
                 return self.read()
